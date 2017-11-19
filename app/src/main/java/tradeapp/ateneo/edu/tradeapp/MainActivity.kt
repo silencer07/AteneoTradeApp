b408b1e7949a3f8b5295de6c9a9fbd9b2e4bde34
Aldrin Tingson <aldrintingson@gmail.com>
2017-11-19 21:21:55 +0800

    added font awesome and google material icons. added top toolbar and sample action

--- app/src/main/java/tradeapp/ateneo/edu/tradeapp/MainActivity.kt
+++ app/src/main/java/tradeapp/ateneo/edu/tradeapp/MainActivity.kt
@@ -1,14 +1,25 @@
 package tradeapp.ateneo.edu.tradeapp
 
+import android.content.Context
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
+import android.support.v4.content.ContextCompat
+import android.support.v4.view.LayoutInflaterCompat
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
+import android.view.Menu
 import android.view.MenuItem
 import android.widget.TextView
-
+import com.mikepenz.iconics.context.IconicsLayoutInflater
 import org.androidannotations.annotations.AfterViews
 import org.androidannotations.annotations.EActivity
 import org.androidannotations.annotations.ViewById
+import android.widget.Toast
+import com.mikepenz.fontawesome_typeface_library.FontAwesome
+import com.mikepenz.iconics.IconicsDrawable
+import com.mikepenz.iconics.context.IconicsContextWrapper
+import org.androidannotations.annotations.OptionsItem
+
 
 @EActivity(R.layout.activity_main)
 open class MainActivity : AppCompatActivity() {
@@ -18,6 +29,9 @@ open class MainActivity : AppCompatActivity() {
 
     @ViewById(R.id.navigation)
     protected lateinit var navigation: BottomNavigationView;
+
+    @ViewById(R.id.toolbar)
+    protected lateinit var toolbar: Toolbar;
 
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
@@ -37,9 +51,34 @@ open class MainActivity : AppCompatActivity() {
         false
     }
 
+    override protected fun attachBaseContext(newBase: Context) {
+        super.attachBaseContext(IconicsContextWrapper.wrap(newBase))
+    }
+
     @AfterViews
     fun setText() {
         navigation!!.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
     }
 
+    @AfterViews
+    fun setToolbar(){
+        setSupportActionBar(toolbar);
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+
+        val actionFavorite = menu.findItem(R.id.action_favorite)
+        val drawable = IconicsDrawable(this);
+        actionFavorite.setIcon(
+                drawable.icon(FontAwesome.Icon.faw_heart).actionBar()
+                        .color(ContextCompat.getColor(this, R.color.colorAccent))
+        )
+        return true
+    }
+
+    @OptionsItem(R.id.action_favorite)
+    fun actionFavoriteClicked(){
+        Toast.makeText(this@MainActivity, "Action clicked", Toast.LENGTH_LONG).show()
+    }
 }
