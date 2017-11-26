dc75666b8a3d5f6db2709beb827ef876aa734229
Aldrin Tingson <aldrintingson@gmail.com>
2017-11-26 19:19:21 +0800

    add comment works. updated gradle

--- app/src/main/java/tradeapp/ateneo/edu/tradeapp/ProductDetailsActivity.kt
+++ app/src/main/java/tradeapp/ateneo/edu/tradeapp/ProductDetailsActivity.kt
@@ -3,20 +3,24 @@ package tradeapp.ateneo.edu.tradeapp
 import android.graphics.drawable.Drawable
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.widget.EditText
 import android.widget.ImageView
-import android.widget.ListView
 import android.widget.TextView
+import android.widget.Toast
 import com.synnapps.carouselview.CarouselView
 import io.realm.Realm
 import io.realm.RealmResults
 import org.androidannotations.annotations.*
+import org.apache.commons.lang3.StringUtils
 import tradeapp.ateneo.edu.tradeapp.adapters.CommentCardAdapter
 import tradeapp.ateneo.edu.tradeapp.model.ActivityWithIconicsContext
 import tradeapp.ateneo.edu.tradeapp.model.Product
 import tradeapp.ateneo.edu.tradeapp.model.ProductComment
+import tradeapp.ateneo.edu.tradeapp.service.UserService
 import java.io.ByteArrayInputStream
 import java.text.DecimalFormat
 import java.text.SimpleDateFormat
+import java.util.*
 
 
 @EActivity(R.layout.activity_product_details)
@@ -49,6 +53,15 @@ open class ProductDetailsActivity : ActivityWithIconicsContext() {
     @ViewById(R.id.commentList)
     lateinit var commentList: RecyclerView
 
+    @ViewById(R.id.addCommentLabel)
+    lateinit var addCommentLabel: TextView
+
+    @ViewById(R.id.addCommentText)
+    lateinit var addCommentText: EditText
+
+    @Bean
+    lateinit var userService: UserService
+
     @AfterViews
     fun initActivity(){
         val product = getProduct()
@@ -69,6 +82,14 @@ open class ProductDetailsActivity : ActivityWithIconicsContext() {
     }
 
     @AfterViews
+    fun hideCommentsIfNotLoggedOn(){
+        if(userService.getLoggedInUser() == null){
+            addCommentLabel.visibility = TextView.INVISIBLE
+            addCommentText.visibility = EditText.INVISIBLE
+        }
+    }
+
+    @AfterViews
     @UiThread
     open fun initComments(){
         commentList.setHasFixedSize(true)
@@ -86,5 +107,26 @@ open class ProductDetailsActivity : ActivityWithIconicsContext() {
 
     private fun getProduct(): Product?{
         return Realm.getDefaultInstance().where(Product::class.java).equalTo("uuid", productUuid).findFirst()
+    }
+
+    @Click(R.id.addCommentBtn)
+    open fun saveComment(){
+        val commentStr = addCommentText.text.toString()
+        if(StringUtils.isBlank(commentStr)){
+            Toast.makeText(this,"Cannot add empty comment", Toast.LENGTH_SHORT).show()
+        } else {
+            Realm.getDefaultInstance().executeTransaction { realm ->
+                val product = getProduct()
+                val comment: ProductComment = realm.createObject(ProductComment::class.java, UUID.randomUUID().toString())
+                comment.text = commentStr
+                comment.product = product
+                comment.user = userService.getLoggedInUser()
+
+                realm.copyToRealm(product)
+            }
+            commentList.adapter.notifyDataSetChanged()
+            Toast.makeText(this,"comment added successfully", Toast.LENGTH_SHORT).show()
+            addCommentText.text.clear()
+        }
     }
 }
