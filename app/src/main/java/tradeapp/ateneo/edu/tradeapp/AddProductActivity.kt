ae2ce361fcf6d911e823e966f2a564f52d11ad4d
Aldrin Tingson <aldrintingson@gmail.com>
2017-11-26 20:19:25 +0800

    added skeleton UI for add product

--- app/src/main/java/tradeapp/ateneo/edu/tradeapp/AddProductActivity.kt
+++ app/src/main/java/tradeapp/ateneo/edu/tradeapp/AddProductActivity.kt
@@ -1,10 +1,22 @@
 package tradeapp.ateneo.edu.tradeapp
 
 import android.support.v7.app.AppCompatActivity
+import android.widget.ArrayAdapter
+import io.realm.Realm
+import io.realm.RealmResults
+import org.androidannotations.annotations.AfterViews
 import org.androidannotations.annotations.EActivity
+import tradeapp.ateneo.edu.tradeapp.model.Category
+import kotlinx.android.synthetic.main.activity_add_product.*
+import org.apache.commons.lang3.StringUtils
 
 @EActivity(R.layout.activity_add_product)
 open class AddProductActivity : AppCompatActivity() {
 
-
+    @AfterViews
+    fun setupCategories(){
+        val categories: RealmResults<Category> = Realm.getDefaultInstance().where(Category::class.java).findAll()
+        val names = categories.map { category -> StringUtils.capitalize(category.name)  }
+        addProductCategorySpinner.adapter = ArrayAdapter(this, R.layout.support_simple_spinner_dropdown_item, names)
+    }
 }
