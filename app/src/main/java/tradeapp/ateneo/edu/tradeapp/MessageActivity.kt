c35550d918c85e82c118938888379845c6aa3d2e
Aldrin Tingson <aldrintingson@gmail.com>
2017-11-29 16:38:31 +0800

    uncomment the code for searching the messages. send message works

--- app/src/main/java/tradeapp/ateneo/edu/tradeapp/MessageActivity.kt
+++ app/src/main/java/tradeapp/ateneo/edu/tradeapp/MessageActivity.kt
@@ -2,15 +2,15 @@ package tradeapp.ateneo.edu.tradeapp
 
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
+import android.widget.Toast
 import io.realm.Realm
 import io.realm.RealmResults
-import org.androidannotations.annotations.AfterViews
-import org.androidannotations.annotations.EActivity
-import org.androidannotations.annotations.Extra
 import kotlinx.android.synthetic.main.activity_message.*
-import org.androidannotations.annotations.Bean
+import org.androidannotations.annotations.*
+import org.apache.commons.lang3.StringUtils
 import tradeapp.ateneo.edu.tradeapp.adapters.MessageListAdapter
 import tradeapp.ateneo.edu.tradeapp.model.ProductComment
+import tradeapp.ateneo.edu.tradeapp.model.User
 import tradeapp.ateneo.edu.tradeapp.model.UserMessage
 import tradeapp.ateneo.edu.tradeapp.service.UserService
 
@@ -28,19 +28,37 @@ open class MessageActivity : AppCompatActivity() {
         val loggedInUser =  userService.getLoggedInUser()
         val realm = Realm.getDefaultInstance()
         val userMessages: RealmResults<UserMessage> = realm.where(UserMessage::class.java)
-//                .beginGroup()
-//                    .equalTo("from.username", username)
-//                    .equalTo("to.username", loggedInUser!!.username)
-//                .endGroup()
-//                .or()
-//                .beginGroup()
-//                    .equalTo("from.username", loggedInUser.username)
-//                    .equalTo("to.username", username)
-//                .endGroup()
+                .beginGroup()
+                    .equalTo("from.username", username)
+                    .equalTo("to.username", loggedInUser!!.username)
+                .endGroup()
+                .or()
+                .beginGroup()
+                    .equalTo("from.username", loggedInUser.username)
+                    .equalTo("to.username", username)
+                .endGroup()
                 .findAllSorted("dateCreated")
         messageList.adapter = MessageListAdapter(this, userMessages)
         messageList.layoutManager = LinearLayoutManager(this);
     }
 
+
+    @UiThread
+    @Click(R.id.sendMessageButton)
+    open fun sendMessage(){
+        if(StringUtils.isNotBlank(messageText.text.toString())){
+            Realm.getDefaultInstance().executeTransaction { realm ->
+                val message = UserMessage()
+                message.from = userService.getLoggedInUser()
+                message.to = realm.where(User::class.java).equalTo("username", username).findFirst()
+                message.text = messageText.text.toString()
+                realm.copyToRealm(message)
+            }
+            setupMessageView()
+            messageText.text.clear()
+        } else {
+            Toast.makeText(this, "Cannot send empty message", Toast.LENGTH_SHORT).show()
+        }
+    }
 
 }
